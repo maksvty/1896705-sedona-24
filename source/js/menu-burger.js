88ce891d161b89c1e8b3226599782d9a37de6b3c
maksvty <yurij1101@gmail.com>
2022-02-02 02:04:49 +0300

    fix

--- source/js/menu-burger.js
+++ source/js/menu-burger.js
@@ -1,5 +1,9 @@
 const button = document.querySelector('.main-nav__toggle');
 const nav = document.querySelector('.main-nav');
+const logo = document.querySelector('.page-header__logo');
+
+nav.classList.add('main-nav--closed');
+logo.classList.add('page-header__logo--order');
 
 button.onclick = function() {
   nav.classList.toggle('main-nav--closed');
